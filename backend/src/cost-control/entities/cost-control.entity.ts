7a929282d1f48d52bae60730e31a22d5091cc2ea
Otávio Júnior <otaviojr08@gmail.com>
2025-02-05 20:03:58 -0300

    feat: add cost-control and quality-control entities

--- backend/src/cost-control/entities/cost-control.entity.ts
+++ backend/src/cost-control/entities/cost-control.entity.ts
@@ -1,1 +1,32 @@
-export class CostControl {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class CostControl {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ nullable: false })
+  cost: number;
+
+  @Column({ nullable: false })
+  productivity: number;
+
+  @Column({ nullable: false })
+  cost_per_bag: number;
+
+  @Column({ nullable: false })
+  sale_price: number;
+
+  @Column({ nullable: false })
+  fertilizer_expenses: number;
+
+  @Column({ nullable: false })
+  labor_cost: number;
+
+  @Column({ nullable: false })
+  irrigation_expenses: number;
+
+  @Column({ nullable: false })
+  transportation_cost: number;
+}
+
--- backend/src/quality-control/entities/quality-control.entity.ts
+++ backend/src/quality-control/entities/quality-control.entity.ts
@@ -1,1 +1,31 @@
-export class QualityControl {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class QualityControl {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ nullable: false })
+  humidity: number;
+
+  @Column({ nullable: false })
+  density: number;
+
+  @Column({ nullable: false })
+  average_grain_size: number;
+
+  @Column({ nullable: false })  
+  impurities: number;
+
+  @Column({ nullable: false })
+  defects: number;
+
+  @Column({ nullable: false })
+  batch_origin: string;
+
+  @Column({ nullable: false })
+  processing_method: string;
+
+  @Column({ nullable: false })
+  sca_score: number;
+}
